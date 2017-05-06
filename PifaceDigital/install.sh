302f81e203551e530ff47039da7584953f362271
FlavienVernier <flavien.vernier@gmail.com>
2017-05-06 17:50:38 +0000

    daemonify PifaceDigital

---
+++ PifaceDigital/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+useradd --system pifacedigital
+usermod -a -G spi pifacedigital
+usermod -a -G gpio pifacedigital
+
+mkdir /var/lib/pifacedigital
+chown -R pifacedigital:pifacedigital /var/lib/pifacedigital
+chmod -R 0755 /var/lib/pifacedigital 
+
+########
+# init.d DOES NOT WORK !!!
+#cp ./etc/init.d/pifacedigital /etc/init.d/pifacedigital
+#chmod 755 /etc/init.d/pifacedigital
+#update-rc.d pifacedigital defaults
+
+#########
+# systemd
+cp pifacedigital /etc/default
+cp pifacedigital.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable pifacedigital
